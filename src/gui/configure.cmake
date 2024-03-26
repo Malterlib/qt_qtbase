506e99cca4ed02bf7c87a61a936a125e415fc3e0
Erik Olofsson <erik@olofsson.info>
2024-03-26 19:53:34 +0100

    Allow system png and jpeg without finding library

--- src/gui/configure.cmake
+++ src/gui/configure.cmake
@@ -923,7 +923,7 @@ qt_feature("jpeg" PRIVATE
 qt_feature_definition("jpeg" "QT_NO_IMAGEFORMAT_JPEG" NEGATE VALUE "1")
 qt_feature("system-jpeg" PRIVATE
     LABEL "  Using system libjpeg"
-    CONDITION QT_FEATURE_jpeg AND JPEG_FOUND
+    CONDITION QT_FEATURE_jpeg
     ENABLE INPUT_libjpeg STREQUAL 'system'
     DISABLE INPUT_libjpeg STREQUAL 'qt'
 )
@@ -935,7 +935,7 @@ qt_feature_definition("png" "QT_NO_IMAGEFORMAT_PNG" NEGATE)
 qt_feature("system-png" PRIVATE
     LABEL "  Using system libpng"
     AUTODETECT QT_FEATURE_system_zlib
-    CONDITION QT_FEATURE_png AND WrapSystemPNG_FOUND
+    CONDITION QT_FEATURE_png
     ENABLE INPUT_libpng STREQUAL 'system'
     DISABLE INPUT_libpng STREQUAL 'qt'
 )
